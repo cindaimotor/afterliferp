b58bd56e8891c3a08f6a905c27ad54ac9fe7ddf4
cindaimotor <43329164+cindaimotor@users.noreply.github.com>
2018-09-16 23:52:14 -0400

    Update client.lua

--- client.lua
+++ client.lua
@@ -1,3 +1,19 @@
-AddEventHandler("playerSpawned", function(spawn)
-    TriggerEvent("chatMessage", "", { 0, 0, 0 }, "Hello world!")
+local spawnPos = vector3(-1037.648, -2737.965, 20.169)
+
+AddEventHandler('onClientGameTypeStart', function()
+    exports.spawnmanager:setAutoSpawnCallback(function()
+        exports.spawnmanager:spawnPlayer({
+            x = spawnPos.x,
+            y = spawnPos.y,
+            z = spawnPos.z,
+            model = 'a_m_m_skater_01'
+        }, function()
+            TriggerEvent('chat:addMessage', {
+                args = { 'Welcome to Afterlife RP' }
+            })
+        end)
+    end)
+
+    exports.spawnmanager:setAutoSpawn(true)
+    exports.spawnmanager:forceRespawn()
 end)
